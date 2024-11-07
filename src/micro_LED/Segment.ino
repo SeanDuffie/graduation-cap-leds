55c87546379cd3e663fc2f950bbcb31994d68e44
SeanDuffie <duffiesean@gmail.com>
2024-11-07 18:02:35 -0500

    More progress on the Segment Object

--- src/micro_LED/Segment.ino
+++ src/micro_LED/Segment.ino
@@ -1,7 +1,32 @@
-class Segment {
+class Sequence {
     public:
-        int dim_x;
-        int dim_y;
-        int fcount;
-        long frames[];
+        // Width of the LED Matrix
+        int dim_x = 16;
+        // Length of the LED Matrix
+        int dim_y = 16;
+        // Number of frames in the Sequence
+        int fcount = 1;
+        // Index of the current frame being displayed
+        int cur = 0;
+        //
+        long frames[fcount][dim_x * dim_y];
+        // Is it a 
+
+        int getDelay();
+        long* nextFrame();
+
+        // Default Constructor
+        Sequence()
 };
+
+Sequence::Sequence() {
+
+}
+
+int Sequence::getDelay() {
+    if 
+}
+
+long* Sequence::nextFrame() {
+    return self.frames[self.cur++];
+}
