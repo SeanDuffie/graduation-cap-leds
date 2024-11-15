3f86a0055bd268330bb1c616bbfbb61e504cada1
SeanDuffie <duffiesean@gmail.com>
2024-11-15 14:10:33 -0500

    Update Sequence with vector functionality

--- src/micro_LED/Sequence.h
+++ src/micro_LED/Sequence.h
@@ -7,22 +7,25 @@
  * The purpose of this project is to create a 
  */
 
+#include <vector>
+
 class Sequence {
     public:
         Sequence();
         Sequence(int dx, int dy);
 
-        int getDelay();
-        long* nextFrame();
-        void insertFrame();
+        float getDelay();
+        std::vector<long> nextFrame();
+        std::vector<long> prevFrame();
+        void insertBatch(std::vector<std::vector<long>> img_arr);
+        void insertFrame(std::vector<long> new_frame);
+        void removeFrame(int index);
 
     private:
         // Width of the LED Matrix
         int dim_x;
         // Length of the LED Matrix
         int dim_y;
-        // Number of frames in the Sequence
-        int fcount;
         // Index of the current frame being displayed
         int cur;
         // Container for frames to be displayed
@@ -33,22 +36,51 @@ class Sequence {
         // int bpm;
 };
 
-Sequence::Sequence() : dim_x(16), dim_y(16), fcount(0), cur(0) {
+Sequence::Sequence() : dim_x(16), dim_y(16), cur(0) {
     frames = {};
 }
 
-Sequence::Sequence(int dx, int dy) : dim_x(dx), dim_y(dy), fcount(0), cur(0) {
+Sequence::Sequence(int dx, int dy) : dim_x(dx), dim_y(dy), cur(0) {
     frames = {};
 }
 
-int Sequence::getDelay() {
-    return 180;
+float Sequence::getDelay() {
+    return 1/180;
 }
 
-long* Sequence::nextFrame() {
-    return self.frames[self.cur++];
+std::vector<long> Sequence::nextFrame() {
+    if (frames.size() == 0) {
+        return;
+    } if (cur >= frames.size()) {
+        cur = 0;
+    } else {
+        cur++;
+    }
+    return frames[cur];
 }
 
-void Sequence::insertFrame() {
+std::vector<long> Sequence::prevFrame() {
+    if (frames.size() == 0) {
+        return;
+    } if (cur >= frames.size()) {
+        cur = 0;
+    } else {
+        cur++;
+    }
+    return frames[cur];
+}
 
+void Sequence::insertBatch(std::vector<std::vector<long>> img_arr) {
+    for (std::vector<long> img : img_arr) {
+        insertFrame(img);
+    }
+}
+
+void Sequence::insertFrame(std::vector<long> new_frame) {
+    frames.insert(frames.end(), new_frame);
+}
+
+void Sequence::removeFrame(int index) {
+    if (index < 0 || index >= frames.size()) {return;}
+    frames.erase(frames.begin() + index);
 }
